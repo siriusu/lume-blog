b58a0496ad19799a4c04e8a6283a1934ceb189c5
siriusu <siriusu09@gmail.com>
2024-07-21 01:11:35 +0900

    set CMS options

--- _cms.ts
+++ _cms.ts
@@ -15,4 +15,55 @@ const cms = lumeCMS({
 	},
 });
 
+cms.document("toppage", "src:index.md", [
+	"title: text",
+	"description: textarea",
+	"content: markdown",
+]);
+cms.collection({
+	name: "posts",
+	description: "To create, edit or delete the posts of the blog",
+	store: "src:posts/*.md",
+	fields: [
+		{
+			name: "title",
+			type: "text",
+			label: "Title of the content",
+			description: "It will be visible in the browser tab",
+			attributes: {
+				required: true,
+				maxlength: 100,
+			},
+		},
+		{
+			name: "published",
+			type: "date",
+			label: "Published date",
+			value: new Date(),
+			description: "Set a future date if you want to publish it later",
+			attributes: {
+				placeholder: "For example: 2024-01-01",
+			},
+		},
+		{
+			name: "tags",
+			type: "list",
+			label: "tags for content",
+			init(field) {
+				const { data } = field.cmsContent;
+				field.options = data.site?.search.values("tags");
+			},
+			description: "Set a tags for Categorize content",
+		},
+		{
+			name: "content",
+			type: "markdown",
+			label: "Page content",
+			value: "Write **markdown** code here",
+			description: `<a target="_blank" href="https://www.markdownguide.org">More info about markdown syntax</a>`,
+		},
+	],
+	nameField: "title",
+});
+
 export default cms;
